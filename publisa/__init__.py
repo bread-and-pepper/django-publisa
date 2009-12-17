b381953a8a33b9ce88aa81b468d7fea15953bd1e
Petar Radosevic <petar@wunki.org>
2009-12-17 15:04:07 +0100

    Better registration of model

--- publisa/__init__.py
+++ publisa/__init__.py
@@ -2,31 +2,24 @@ from django.utils.translation import ugettext as _
 
 from publisa.models import PublishDescriptor
 
-class AlreadyRegistered(Exception):
-    """
-    An attempt was made to register a model for Publisher more than once.
-    """
-    pass
-
 registry = []
 def register(model, allow_banners=True):
     """ Setup a model to be managed by Publisa. """
 
-    if model in registry:
-        raise AlreadyRegistered(_('The model %s has already been registered.') % model.__name__)
+    if model not registry:
+        # Set the extra attributes
+        setattr(model, 'allow_banners', allow_banners)
+        setattr(model, 'publish', PublishDescriptor())
+
+        # If the model has no status, always see it as finished
+        if not hasattr(model, 'status'):
+            setattr(model, 'status', 2)
+
+        # Send a signal to our receivers when the model is updated or deleted.
+        from django.db.models import signals
+        from publisa.signals import post_save_handler, post_delete_handler
+
+        signals.post_save.connect(post_save_handler, sender=model)
+        signals.post_delete.connect(post_delete_handler, sender=model)
+
     registry.append(model)
-
-    # Set the extra attributes
-    setattr(model, 'allow_banners', allow_banners)
-    setattr(model, 'publish', PublishDescriptor())
-
-    # If the model has no status, always see it as finished
-    if not hasattr(model, 'status'):
-        setattr(model, 'status', 2)
-
-    # Send a signal to our receivers when the model is updated or deleted.
-    from django.db.models import signals
-    from publisa.signals import post_save_handler, post_delete_handler
-
-    signals.post_save.connect(post_save_handler, sender=model)
-    signals.post_delete.connect(post_delete_handler, sender=model)
