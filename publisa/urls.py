e13933f629a7969453a37fcd50794a52d7fc2f71
Petar Radosevic <petar@wunki.org>
2010-01-05 16:02:35 +0100

    Fixed pagination

--- publisa/urls.py
+++ publisa/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('publisa.views',
     url(r'^page/(?P<page>\w)/$',
         view='index',
-        name='publish-index-paginated'),
+        name='publisa-index-paginated'),
     url('^$',
        name='publisa-index',
        view='index',)
